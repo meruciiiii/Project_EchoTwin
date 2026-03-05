dacbc7a7bc8ff4c30d06307bc702482c56343c3c
meruciiiii <127821538+meruciiiii@users.noreply.github.com>
2026-03-05 12:40:05 +0900

    사운드몬스터 제작중

--- Assets/3.Script/Weapon/Ji/OrbitAxe.cs
+++ Assets/3.Script/Weapon/Ji/OrbitAxe.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class OrbitAxe : MonoBehaviour
@@ -10,24 +7,28 @@ public class OrbitAxe : MonoBehaviour
     [SerializeField] private float speed = 5f;
     [SerializeField] private float radius = 2f;
     [SerializeField] private float rotateSpeed = 180f;
+    [SerializeField] private GameObject hitEffectPrefab; 
     private float damage;
+
+    private float currentYRotation;
 
     public void Init(Transform player, float damage)
     {
         this.player = player;
         this.damage = damage;
         angle = 0f;
+        currentYRotation = 0f;
     }
 
     private void Update()
     {
         angle += Time.deltaTime * speed;
-
         transform.position = player.position + new Vector3(Mathf.Cos(angle), 0, Mathf.Sin(angle)) * radius;
 
-        transform.Rotate(0f, rotateSpeed * Time.deltaTime, 0f, Space.Self);
+        currentYRotation += rotateSpeed * Time.deltaTime;
+        transform.rotation = Quaternion.Euler(90f, currentYRotation, 0f);
 
-        if(angle >= Mathf.PI * 2f)
+        if (angle >= Mathf.PI * 2f)
         {
             Destroy(gameObject);
         }
@@ -35,10 +36,17 @@ public class OrbitAxe : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("Enemy"))
+        if (other.CompareTag("Enemy"))
         {
-            //other.getcomponent<enemy>().takedamage(damage);
             other.GetComponent<EnemyStateAbstract>().takeDamage(damage);
+
+            SoundManager.SendEvent(SoundType.SFX_AxeAttack1);
+
+            if (hitEffectPrefab != null)
+            {
+                Vector3 hitPos = other.ClosestPoint(transform.position); // 가장 가까운 접점 찾기
+                Instantiate(hitEffectPrefab, hitPos, Quaternion.identity);
+            }
         }
     }
 }
